0bb7978c2fdb5b46abd3f77b18e06f295465369e
Megan Nickerson <megannickerson@Megans-MacBook-Air.local>
2021-10-27 10:40:42 -0700

    in-class changes

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -6,9 +6,12 @@ Purpose: Find common kmers
 """
 
 import argparse
-
+import io
+from typing import DefaultDict
 
 # --------------------------------------------------
+
+
 def get_args():
     """Get command-line arguments"""
 
@@ -46,33 +49,18 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    filename1 = args.File1
-    filename2 = args.File2
-    kmers = args.kmer
 
-    kmers1 = count_kmers(filename1, kmers)
-    kmers2 = count_kmers(filename2, kmers)
+    kmers1 = count_kmers(args.File1, args.kmer)
+    kmers2 = count_kmers(args.File2, args.kmer)
 
-    common = sorted(list(set(kmers1).intersection(kmers2)))
-    for kmer in common:
+    for common in set(kmers1).intersection(set(kmers2)):
         print('{:10}{:6}{:6}'.format(
-            kmer, kmers1.get(kmer), kmers2.get(kmer)), end='\n')
-# --------------------------------------------------
+            common, kmers1.get(common), kmers2.get(common)))
 
-
-def count_kmers(filename, k):
-    """Count kmers in a file to create a dictionary"""
-
-    words = {}
-    for line in filename:
-        for word in line.split():
-            for kmer in find_kmers(word, k):
-                if kmer in words:
-                    words[kmer] += 1
-                else:
-                    words[kmer] = 1
-
-    return words
+    # common = sorted(list(set(kmers1).intersection(kmers2)))
+    # for kmer in common:
+    #     print('{:10}{:6}{:6}'.format(
+    #         kmer, kmers1.get(kmer), kmers2.get(kmer)), end='\n')
 
 # --------------------------------------------------
 
@@ -83,6 +71,56 @@ def find_kmers(seq, k):
     n = len(seq) - k + 1
 
     return [] if n < 1 else [seq[i:i + k] for i in range(n)]
+
+# --------------------------------------------------
+
+
+def test_find_kmers():
+    """ Test find_kmers """
+
+    assert find_kmers('', 1) == []
+    assert find_kmers('ACTG', 1) == ['A', 'C', 'T', 'G']
+    assert find_kmers('ACTG', 2) == ['AC', 'CT', 'TG']
+    assert find_kmers('ACTG', 3) == ['ACT', 'CTG']
+    assert find_kmers('ACTG', 4) == ['ACTG']
+    assert find_kmers('ACTG', 5) == []
+# --------------------------------------------------
+
+
+def count_kmers(filename, k):
+    """Count kmers in a file to create a dictionary"""
+
+    # words = {}
+    # for line in filename:
+    #     for word in line.split():
+    #         for kmer in find_kmers(word, k):
+    #             if kmer in words:
+    #                 words[kmer] += 1
+    #             else:
+    #                 words[kmer] = 1
+
+    kmers = DefaultDict(int)
+    for line in filename:
+        for word in line.split():
+            for kmer in find_kmers(word, k):
+                kmers[kmer] += 1
+
+    return kmers
+# --------------------------------------------------
+
+
+def test_count_kmers():
+    """Test count kmers"""
+    dat = 'foo\nbar\nbaz\n'
+
+    assert count_kmers(io.StringIO(dat), 3) == {'foo': 1, 'bar': 1, 'baz': 1}
+    assert count_kmers(io.StringIO(dat), 2) == {
+        'fo': 1,
+        'oo': 1,
+        'ba': 2,
+        'ar': 1,
+        'az': 1
+    }
 
 
 # --------------------------------------------------
