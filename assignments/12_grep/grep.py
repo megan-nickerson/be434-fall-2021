1e70c786bb41383e5dc0fe1e87310fc2193e91e0
Megan Nickerson <megannickerson@Megans-MacBook-Air.local>
2021-11-23 18:40:57 -0700

    complete

--- assignments/12_grep/grep.py
+++ assignments/12_grep/grep.py
@@ -7,9 +7,11 @@ Purpose: Print all the lines from files that match provided regex
 
 import argparse
 import sys
-
+import re
 
 # --------------------------------------------------
+
+
 def get_args():
     """Get command-line arguments"""
 
@@ -21,16 +23,17 @@ def get_args():
                         metavar='PATTERN',
                         help='Search pattern')
 
-    parser.add_argument('file',
-                        help='Input file(s)',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default='')
+    # parser.add_argument('file',
+    #                     help='Input file(s)',
+    #                     nargs='+',
+    #                     metavar='FILE',
+    #                     type=argparse.FileType('rt'),
+    #                     default='')
 
     parser.add_argument('-i',
                         '--insensitive',
                         help='Case-insensitive search',
-                        metavar='',
+                        action='store_true',
                         default=False)
 
     parser.add_argument('-o',
@@ -40,6 +43,14 @@ def get_args():
                         type=argparse.FileType('wt'),
                         default=sys.stdout)
 
+    parser.add_argument('file',
+                        help='Input file(s)',
+                        nargs='+',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default='')
+# putting file at the end gives the right order of outputs from the second hint
+
     return parser.parse_args()
 
 
@@ -48,8 +59,36 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print(args)
+    # print(args)
+    motif = args.pattern
+    input_file = args.file
+    case_insensitive = args.insensitive
+
+    for filename in input_file:
+        for line in filename:
+            search = re.search(motif, line,
+                               re.I) if case_insensitive else re.search(
+                motif, line)
+            if search:
+                if len(input_file) > 1:
+                    print(filename.name + ':' + line,
+                          file=args.outfile, end='')
+                else:
+                    print(line, file=args.outfile, end='')
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
+    # for filename in input_file:
+    #     if case_insensitive:
+    #         search = re.search(motif, line, re.I)
+    #     else:
+    #         search = re.search(motif, line)
+    #     if search:
+    #         if len(input_file) > 1:
+    #             print(filename.name + ':' + line, file=args.outfile, end='')
+    #         else:
+    #             print(line, file=args.outfile, end='')
+    # search = re.search(motif, input_file)
+    # print(search)
